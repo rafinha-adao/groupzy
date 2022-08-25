359a0a1969eec63dd4d30baef16b9516ad89a885
Rafael Adão <raphinha.oliveira11@gmail.com>
2022-08-25 16:09:50 -0300

    style: aplicar nova border-r em toda a aplicação

--- assets/js/homeVerify.js
+++ assets/js/homeVerify.js
@@ -1,9 +1,10 @@
-if (sessionStorage.getItem('login')) {
-    document.getElementById('btn-login').style.display = 'none';
-    document.getElementById('btn-signup').style.display = 'none';
+if (sessionStorage.getItem("login")) {
+    document.getElementById("btn-login").style.display = "none";
+    document.getElementById("btn-signup").style.display = "none";
 }
-if (!sessionStorage.getItem('login')) {
-    document.getElementById('btn-logout').style.display = 'none';
-    document.getElementById('btn-chat').style.display = 'none';
-    document.getElementById('btn-groups').style.display = 'none';
+if (!sessionStorage.getItem("login")) {
+    document.getElementById("btn-chat").style.display = "none";
+    document.getElementById("btn-groups").style.display = "none";
+    document.getElementById("btn-profile").style.display = "none";
+    document.getElementById("btn-logout").style.display = "none";
 }
