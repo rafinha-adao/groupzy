d89784d6e690a8025fc4fd81240c1916d964b191
Rafael Adão <raphinha.oliveira11@gmail.com>
2023-05-07 13:31:36 -0300

    fix: change env file API_URL

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,5 +1,5 @@
-// DEV
-//const API_URL = 'http://localhost:3000/';
+/**
+ * dev: http://localhost:3000/
+ */
 
-// PROD
-const API_URL = 'https://groupzy.rafinhaadao.com/groupzy/api/';
+const API_URL = 'https://groupzy.rafinhaadao.com/api/';
